f39543c592dbc637fd2a96ed3f701a06991731b8
Mankevich Stanislav <zstreetfox@gmail.com>
2020-02-07 18:18:45 +0200

    рабочая версия

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
 'use strict';
-// function guess() {
-//     let isNumber = function(n) {
-//     return !isNaN(parseFloat(n)) && isFinite(n)
-//     };
 
 let books = document.querySelectorAll('.books'),
     book = document.querySelectorAll('.book'),
@@ -14,8 +10,7 @@ let books = document.querySelectorAll('.books'),
     li = document.querySelectorAll('li'),
     div = document.querySelector('div'),
     a = document.querySelectorAll('a');
-   
-    
+
     bgImage.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg);');   
     books[0].insertBefore(book[1],book[0]);
     books[0].insertBefore(book[4],book[2]);
@@ -27,18 +22,30 @@ let books = document.querySelectorAll('.books'),
     
     a[4].innerHTML = a[4].innerHTML.replace(/Книга 3. this и Пропопипы Объектов/,'Книга 3. this и Прототипы Объектов!');
 
-   
     ul[0].insertBefore(li[2],li[10]);
     ul[0].insertBefore(li[6],li[4]);
     ul[0].insertBefore(li[8],li[4]);
-  
-   
- console.log(ul,li);
-
 
 function newFunction() {
     ul[5].insertBefore(li[55], li[48]);
     ul[5].insertBefore(li[48], li[52]);
     ul[5].insertBefore(li[51], li[54]);
-}
+};
 newFunction();
+ 
+
+let newElem = document.createElement('li');
+newElem.textContent = 'Глава 8: За пределами ES6';
+ul[2].appendChild(newElem);
+function insertBook() {
+
+    ul[2].insertBefore(newElem, li[26]);
+
+    function newFunction() {
+        return ul[2];
+    }
+};
+insertBook();
+console.log(li);
+
+
